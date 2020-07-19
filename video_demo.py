2f9eeb6e7d72dcbc4cb8e870894646b870686650
Vaibhav Saxena <30471218+vaib-saxena@users.noreply.github.com>
2020-07-19 15:56:20 +0530

    Add files via upload
    video_demo update

--- video_demo.py
+++ video_demo.py
@@ -1,10 +1,10 @@
 from __future__ import division
 import time
-import torch 
+import torch
 import copy
 from torch.autograd import Variable
 import numpy as np
-import cv2 
+import cv2
 from util import *
 from darknet import Darknet
 from preprocess import prep_image, inp_to_image, letterbox_image
@@ -19,6 +19,8 @@ from config.train_config import TrainConfig
 from config.eval_config import EvalConfig
 from config.config import Config
 from model import Model
+import csv
+import pandas as pd
 
 def index2class():
     file_path = '/content/Slow-Fast-pytorch-implementation/ava/ava_action_list_v2.0.csv'
@@ -29,22 +31,22 @@ def index2class():
 
 def get_test_input(input_dim, CUDA):
     img = cv2.imread("/content/Slow-Fast-pytorch-implementation/imgs/dog.jpg")
-    img = cv2.resize(img, (input_dim, input_dim)) 
+    img = cv2.resize(img, (input_dim, input_dim))
     img_ =  img[:,:,::-1].transpose((2,0,1))
     img_ = img_[np.newaxis,:,:,:]/255.0
     img_ = torch.from_numpy(img_).float()
     img_ = Variable(img_)
-    
+
     if CUDA:
         img_ = img_.cuda()
-    
+
     return img_
 
 def prep_image(img, inp_dim):
     """
-    Prepare image for inputting to the neural network. 
-    
-    Returns a Variable 
+    Prepare image for inputting to the neural network.
+
+    Returns a Variable
     """
 
     orig_im = img
@@ -66,22 +68,34 @@ def to_tensor(buffer):
     # D = Depth (in this case, time), H = Height, W = Width, C = Channels
     return buffer.transpose([3, 0, 1, 2])
 
+para = []
 def imshow(bboxes, labels, probs,ids,count):
 
+
     for bbox, lables, prob,i in zip(bboxes, labels, probs,ids):
+      #with open('activity1.csv', 'w', newline='') as file:
+        #writer = csv.writer(file)
+        #writer.writerow([round(p, 2), index2class()[str(lable)].split("(")[0], i])
+
         count_2 = 0
         for lable, p in zip(lables, prob):
             count_2 = count_2 + 1
             bbox = np.array(bbox)
             lable = int(lable)
             p = float(p)
+            rat = (ZZ,i, round(p, 2),index2class()[str(lable)].split("(")[0])
+            para.append(rat)
+            #print(para)
+            #print(index2class()[str(lable)].split("(")[0])
+            #print(i)
+            #print(frame)
             real_x_min = int(bbox[0])
             real_y_min = int(bbox[1])
             real_x_max = int(bbox[2])
             real_y_max = int(bbox[3])
             # Draw rectangle on each frame, frame frame, (four coordinate parameters), (color), width
             cv2.rectangle(frame, (real_x_min, real_y_min), (real_x_max, real_y_max), (0, 255, 255),
-                          4)  # 红色
+                          2)  # yellow color
             cv2.putText(frame, index2class()[str(lable)].split("(")[0] + ':' + str(round(p, 2)),
                         (real_x_min + 15, real_y_max - 15 * count_2),
                         cv2.FONT_HERSHEY_COMPLEX, \
@@ -90,37 +104,40 @@ def imshow(bboxes, labels, probs,ids,count):
                         (real_x_min + 10, real_y_min + 20),
                         cv2.FONT_HERSHEY_COMPLEX, \
                         0.5, (0, 255, 255), 1, False)
-
+        column_names = ['Frame', 'Person ID', 'Probability', 'Action Class']
+        data = np.array(para)
+        my_df = pd.DataFrame(data, columns=column_names)
+        my_df.to_csv('/content/drive/My Drive/file_name.csv')
         #you can change the output folder for saving the frames
-        cv2.imwrite('/content/Slow-Fast-pytorch-implementation/outputs/frames/%d.jpg' % count, frame)
+        cv2.imwrite('/content/drive/My Drive/outputs/frames/%d.jpg' % count, frame)
 
 def arg_parse():
     """
     Parse arguements to the detect module
-    
+
     """
-    
-    
+
+
     parser = argparse.ArgumentParser(description='YOLO v3 Video Detection Module')
-   	
+
    	#change the video path accordingly
-    parser.add_argument("--video", dest = 'video', help = 
+    parser.add_argument("--video", dest = 'video', help =
                         "Video to run detection upon",
                         default = "/content/Slow-Fast-pytorch-implementation/new.mp4", type = str)
     parser.add_argument("--dataset", dest = "dataset", help = "Dataset on which the network has been trained", default = "pascal")
     parser.add_argument("--confidence", dest = "confidence", help = "Object Confidence to filter predictions", default = 0.5)
     parser.add_argument("--nms_thresh", dest = "nms_thresh", help = "NMS Threshhold", default = 0.4)
-    
+
     #change the path accordingly
-    parser.add_argument("--cfg", dest = 'cfgfile', help = 
+    parser.add_argument("--cfg", dest = 'cfgfile', help =
                         "Config file",
                         default = "/content/Slow-Fast-pytorch-implementation/cfg/yolov3.cfg", type = str)
-    
+
     #change the path accordingly
-    parser.add_argument("--weights", dest = 'weightsfile', help = 
+    parser.add_argument("--weights", dest = 'weightsfile', help =
                         "weightsfile",
                         default = "/content/Slow-Fast-pytorch-implementation/yolov3.weights", type = str)
-    parser.add_argument("--reso", dest = 'reso', help = 
+    parser.add_argument("--reso", dest = 'reso', help =
                         "Input resolution of the network. Increase to increase accuracy. Decrease to increase speed",
                         default = "416", type = str)
     return parser.parse_args()
@@ -137,15 +154,15 @@ if __name__ == '__main__':
     num_classes = 80
 
     CUDA = torch.cuda.is_available()
-    
+
     bbox_attrs = 5 + num_classes
-    
+
     #change the path accordingly
     print("Loading network.....")
     model = Darknet("/content/Slow-Fast-pytorch-implementation/cfg/yolov3.cfg")
     model.load_weights("/content/Slow-Fast-pytorch-implementation/yolov3.weights")
     print("Network successfully loaded")
-    
+
     #change the path accordingly
     print("load deep sort network....")
     deepsort = DeepSort("/content/Slow-Fast-pytorch-implementation/deep/checkpoint/ckpt.t7")
@@ -159,7 +176,7 @@ if __name__ == '__main__':
     if CUDA:
         model.cuda()
 
-        
+
     # model(get_test_input(inp_dim, CUDA), CUDA)
 
     model.eval()
@@ -179,9 +196,9 @@ if __name__ == '__main__':
     #Video file on which you want to run the model
     videofile = "/content/Slow-Fast-pytorch-implementation/new.mp4"
     cap = cv2.VideoCapture(videofile)
-    
+
     assert cap.isOpened(), 'Cannot capture source'
-    
+
     frames = 0
     last = np.array([])
     last_time = time.time()
@@ -195,6 +212,7 @@ if __name__ == '__main__':
 
 
     count=0
+    ZZ = 0
     while cap.isOpened():
 
         ret, frame = cap.read()
@@ -212,36 +230,38 @@ if __name__ == '__main__':
             scale = [resize_width / frame_width, resize_height / frame_height]
 
             img, orig_im, dim = prep_image(frame, inp_dim)
-            
-            im_dim = torch.FloatTensor(dim).repeat(1,2)                        
-            
-            
+
+            im_dim = torch.FloatTensor(dim).repeat(1,2)
+
+
             if CUDA:
                 im_dim = im_dim.cuda()
                 img = img.cuda()
-            
-            with torch.no_grad():   
+
+            with torch.no_grad():
                 output = model(Variable(img), CUDA)
             output = write_results(output, confidence, num_classes, nms = True, nms_conf = nms_thesh)
 
             if type(output) == int:
                 frames += 1
+                ZZ+= 1
                 print("FPS of the video is {:5.2f}".format( frames / (time.time() - start)))
+                print(ZZ)
                 #cv2.imshow("frame", orig_im)
                 key = cv2.waitKey(1)
                 if key & 0xFF == ord('q'):
                     break
                 continue
-            
+
 
             im_dim = im_dim.repeat(output.size(0), 1)
             scaling_factor = torch.min(inp_dim/im_dim,1)[0].view(-1,1)
-            
+
             output[:,[1,3]] -= (inp_dim - scaling_factor*im_dim[:,0].view(-1,1))/2
             output[:,[2,4]] -= (inp_dim - scaling_factor*im_dim[:,1].view(-1,1))/2
 
             output[:,1:5] /= scaling_factor
-    
+
             for i in range(output.shape[0]):
                 output[i, [1,3]] = torch.clamp(output[i, [1,3]], 0.0, im_dim[i,0])
                 output[i, [2,4]] = torch.clamp(output[i, [2,4]], 0.0, im_dim[i,1])
@@ -300,12 +320,9 @@ if __name__ == '__main__':
             if key & 0xFF == ord('q'):
                 break
             frames += 1
+            ZZ += 1
             print("FPS of the video is {:5.2f}".format( frames / (time.time() - start)))
+            print(ZZ)
 
         else:
             break
-    
-
-    
-    
-
